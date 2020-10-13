761243524ac300641c2225357ab603a4b6ee17c4
cheerfulleg <61907072+cheerfulleg@users.noreply.github.com>
2020-10-13 22:47:04 +0300

    Updates

---
+++ precache-manifest.8db727d82709049e3265959d69e378b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15437a750c0b07980502bba3e155b7ac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d6607df042094bca37af",
+    "url": "/monsters-rolodex/static/css/main.8abe269c.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6607df042094bca37af",
+    "url": "/monsters-rolodex/static/js/main.37fb1efe.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
